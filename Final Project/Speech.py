ebde57bbdaedfded9e66ef5e7aead094997ead4e
CodeAX2 <jacobhofer1@gmail.com>
2022-05-05 12:52:37 -0600

    Fix speech input initializer

--- Final Project/Speech.py
+++ Final Project/Speech.py
@@ -1,13 +1,15 @@
 import speech_recognition as sr
 
-source = sr.Microphone()
-r = sr.Recognizer()
-r.adjust_for_ambient_noise(source)
 
-def getSpeechInput():
-        try:
-            audio = r.listen(source)            
-            word = r.recognize_google(audio)
-            return str.lower(word).split(" ")
-        except sr.UnknownValueError:
-            return []
+with sr.Microphone as source:
+
+    r = sr.Recognizer()
+    r.adjust_for_ambient_noise(source)
+
+    def getSpeechInput():
+            try:
+                audio = r.listen(source)            
+                word = r.recognize_google(audio)
+                return str.lower(word).split(" ")
+            except sr.UnknownValueError:
+                return []
